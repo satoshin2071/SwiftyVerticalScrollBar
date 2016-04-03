6c57a8794cd66e22a1b9786e9c10545601a40fa6
satoshin2071 <satoshin2071@users.noreply.github.com>
2016-04-03 12:33:15 +0900

    initial commit

---
+++ SwiftyVerticalScrollBar/DemoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DemoViewController.swift
+//  SwiftyVerticalScrollBar
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private (set) var scrollBar : SwiftyVerticalScrollBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.scrollBar = SwiftyVerticalScrollBar(frame: CGRectZero, targetScrollView: self.tableView)
+        self.view.addSubview(self.scrollBar!)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.scrollBar.frame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 1000
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("DemoCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "cell : \(indexPath.row) "
+        return cell
+    }
+}
